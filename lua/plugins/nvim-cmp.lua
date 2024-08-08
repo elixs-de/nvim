5e4035427a946a0137c9b6deffc5c1f0c7ea7b49
TheCodingSpace <mail@elias-dieter.de>
2024-08-08 04:37:53 +0000

    split example.lua and adding cpp

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,8 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = { "hrsh7th/cmp-emoji" },
+  opts = function(_, opts)
+    table.insert(opts.sources, { name = "emoji" })
+    table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+  end,
+}
